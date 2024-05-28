ceadd7656d4c7fbb703146dbfd3e535b67ecf2a1
mahmood100100 <135098525+mahmood100100@users.noreply.github.com>
2024-05-28 15:13:34 +0300

    edit profile picture field name

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -4,7 +4,7 @@ import upload from '../uploadMiddleware.js';
 
 const router = express.Router();
 
-router.post('/register', upload.single('profileImage'), registerUser);
+router.post('/register', upload.single('profilePicture'), registerUser);
 router.post('/login', loginUser);
 
 export default router;
